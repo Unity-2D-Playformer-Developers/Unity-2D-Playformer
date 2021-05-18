94f5f953230f1b219550d2a0553be0922af6afa9
Ys <miszu95@gmail.com>
2021-05-18 15:20:31 +0200

    sp

--- 2D_PlayFormer_Game/Assets/Scripts/PlayerControls.cs
+++ 2D_PlayFormer_Game/Assets/Scripts/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Player/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
