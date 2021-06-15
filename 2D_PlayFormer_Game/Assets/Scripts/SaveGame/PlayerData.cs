2f24bccac117f0b576b3352f15167c02b79d9a5b
savelyevlad <savelyevlad@gmail.com>
2021-06-15 14:40:34 +0200

    Enemy saving

--- 2D_PlayFormer_Game/Assets/Scripts/SaveGame/PlayerData.cs
+++ 2D_PlayFormer_Game/Assets/Scripts/SaveGame/PlayerData.cs
@@ -8,10 +8,27 @@ public class PlayerData
 
     public float x, y;
 
-        public PlayerData(OpenScreen openScreen)
+    public string[] enemiesNames;
+    public float[] enemiesX;
+    public float[] enemiesY;
+    public float[] enemiesZ;
+
+    public PlayerData(OpenScreen openScreen)
     {
         Level = openScreen.Scene();
         GameObject player = GameObject.FindGameObjectsWithTag("Player")[1];
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        enemiesX = new float[enemies.Length];
+        enemiesY = new float[enemies.Length];
+        enemiesZ = new float[enemies.Length];
+        enemiesNames = new string[enemies.Length];
+        for(int i = 0; i < enemies.Length; ++i)
+        {
+            enemiesNames[i] = enemies[i].name;
+            enemiesX[i] = enemies[i].transform.position.x;
+            enemiesY[i] = enemies[i].transform.position.y;
+            enemiesZ[i] = enemies[i].transform.position.z;
+        }
         x = player.transform.position.x;
         y = player.transform.position.y;
     }
--- 2D_PlayFormer_Game/Assets/Scripts/UIScripts/OpenScreen.cs
+++ 2D_PlayFormer_Game/Assets/Scripts/UIScripts/OpenScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -46,6 +47,53 @@ public class OpenScreen : MonoBehaviour
         Debug.Log("OnSceneLoaded: " + scene.name);
         Transform kek = GameObject.FindGameObjectsWithTag("Player")[1].transform;
         kek.position = new Vector2(playerData.x, playerData.y);
+
+        SavedEnemy[] savedEnemies = new SavedEnemy[playerData.enemiesNames.Length];
+
+        int i;
+        int j = 0;
+
+        for (i = 0; i < playerData.enemiesNames.Length; ++i)
+        {
+            savedEnemies[i] = new SavedEnemy();
+            savedEnemies[i].enemyName = playerData.enemiesNames[i];
+            savedEnemies[i].enemyX = playerData.enemiesX[i];
+            savedEnemies[i].enemyY = playerData.enemiesY[i];
+            savedEnemies[i].enemyZ = playerData.enemiesZ[i];
+        }
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Array.Sort(enemies, compareObjects);
+        Array.Sort(savedEnemies);
+
+        for(i = 0; i < playerData.enemiesNames.Length; ++i, ++j)
+        {
+            if(enemies[j].name != savedEnemies[i].enemyName)
+            {
+                Destroy(enemies[j].gameObject);
+                --i;
+                continue;
+            }
+            enemies[j].transform.position = new Vector3(savedEnemies[i].enemyX, savedEnemies[i].enemyY, savedEnemies[i].enemyZ);
+        }
     }
 
+    public int compareObjects(GameObject a, GameObject b)
+    {
+        return a.name.CompareTo(b.name);
+    }
+
+    class SavedEnemy : IComparable
+    {
+        public string enemyName;
+        public float enemyX;
+        public float enemyY;
+        public float enemyZ;
+
+        public int CompareTo(object obj)
+        {
+            SavedEnemy o = (SavedEnemy)obj;
+            return enemyName.CompareTo(o.enemyName);
+        }
+    }
 }
